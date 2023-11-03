1a2a16821a893ff03a55ca3bda97b27093112e2a
Kay Butter <kay@kaybutter.de>
2023-11-03 13:42:06 +0100

    Remove os checks

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "ApolloCodegen",
   platforms: [
-    .macOS(.v12)
+    .macOS(.v12), .iOS(.v15)
   ],
   products: [
     .library(name: "ApolloCodegenLib", targets: ["ApolloCodegenLib"]),

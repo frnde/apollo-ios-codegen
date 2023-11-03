bcd1739f37ceffae6900d5f20088f6493c9a61e0
Kay Butter <kay@kaybutter.de>
2023-11-03 13:42:06 +0100

    Remove os checks

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "ApolloCodegen",
   platforms: [
-    .macOS(.v12)
+    .macOS(.v12), .iOS(.v15)
   ],
   products: [
     .library(name: "ApolloCodegenLib", targets: ["ApolloCodegenLib"]),
--- Sources/ApolloCodegenLib/ApolloCodegen.swift
+++ Sources/ApolloCodegenLib/ApolloCodegen.swift
@@ -4,9 +4,6 @@ import GraphQLCompiler
 import OrderedCollections
 import Utilities
 
-// Only available on macOS
-#if os(macOS)
-
 /// A class to facilitate running code generation
 public final class ApolloCodegen: Sendable {
 
@@ -701,5 +698,3 @@ extension ApolloCodegen {
     }
   }
 }
-
-#endif

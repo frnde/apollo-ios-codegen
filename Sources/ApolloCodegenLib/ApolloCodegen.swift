85915e099212d30b3c898ecdb35fa2a544bc761a
Kay Butter <kay@kaybutter.de>
2023-11-03 13:42:06 +0100

    Remove os checks

--- Sources/ApolloCodegenLib/ApolloCodegen.swift
+++ Sources/ApolloCodegenLib/ApolloCodegen.swift
@@ -4,9 +4,6 @@ import GraphQLCompiler
 import OrderedCollections
 import Utilities
 
-// Only available on macOS
-#if os(macOS)
-
 /// A class to facilitate running code generation
 public class ApolloCodegen {
 
@@ -587,5 +584,3 @@ extension ApolloCodegen {
     }
   }
 }
-
-#endif
